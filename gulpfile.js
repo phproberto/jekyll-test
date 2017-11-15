7447f56eb8d734b7ad0501495e3fae017ea8d12b
Roberto Segura - phproberto <roberto@phproberto.com>
2017-11-15 16:25:58 +0100

    Test new layout

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,29 @@ const child = require('child_process');
 const browserSync = require('browser-sync').create();
 const siteRoot = '_site';
 const cssFiles = 'src/style.css';
+const tailwindConfig = 'tailwind-config.js';
 
-gulp.task('jekyll', () => {
-  const jekyll = child.spawn('jekyll', ['serve',
-    '--watch',
-    '--incremental',
-    '--drafts'
-  ]);
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
-  const jekyllLogger = (buffer) => {
-    buffer.toString()
-      .split(/\n/)
-      .forEach((message) => gutil.log('Jekyll: ' + message));
-  };
-
-  jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify('Running: $ jekyll build');
+    return child.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
 });
 
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Compile styles
+ */
 gulp.task('css', function () {
   const atimport = require('postcss-import');
   const postcss = require('gulp-postcss');
@@ -29,20 +34,24 @@ gulp.task('css', function () {
   const autoprefixer = require('gulp-autoprefixer');
   const cleancss = require('gulp-clean-css');
 
-  return gulp.src('src/style.css')
+  return gulp.src(cssFiles)
     .pipe(postcss([
       atimport(),
-      tailwindcss('tailwind-config.js')
+      tailwindcss(tailwindConfig)
     ]))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cleancss())
+    .pipe(gulp.dest('_site/assets/css/'))
+    .pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest('assets/css/'));
 });
 
-
+/**
+ * Serve site with browserSync
+ */
 gulp.task('serve', () => {
   browserSync.init({
     files: [siteRoot + '/**'],
@@ -52,7 +61,8 @@ gulp.task('serve', () => {
     }
   });
 
-  gulp.watch(cssFiles);
+  gulp.watch([cssFiles, tailwindConfig], ['css']);
+  gulp.watch(['**/*.html', '**/*.yml', '!_site/**/*'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['css', 'jekyll', 'serve']);
+gulp.task('default', ['css', 'serve']);
